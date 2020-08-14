dac7f0670736fda5c703d1274aa2ef639a5ad9cb
ChrisTineo <christophertineo02@gmail.com>
2020-08-13 23:16:54 -0400

    Just rotating for (no drive)

--- NEMA_24.ino
+++ NEMA_24.ino
@@ -1,25 +1,20 @@
-
 int DIR = 8;
 int PUL = 9;
 
 void setup (){
 
-pinMode (DIR, OUTPUT);
-pinMode (PUL, OUTPUT);
+    pinMode (DIR, OUTPUT);
+    pinMode (PUL, OUTPUT);
 
-
-
+    digitalWrite (DIR, HIGH);
 }
 
 void loop (){
 
-digitalWrite (DIR, HIGH);
-for (int i= 0; i <200; i ++);
-digitalWrite (PUL, HIGH);
-delay(1);
-
-digitalWrite(PUL,LOW);
-delay(1);
-
-
+    for (int i = 0; i < 200; i++){
+        digitalWrite (PUL, HIGH);
+        delay(1);
+        digitalWrite(PUL,LOW);
+        delay(1);
+    }
 }
